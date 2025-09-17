0483085c6f50da666eea7977a26aca4010c17e88
Robert Whisenhunt <dahweirdacus@gmail.com>
2025-09-17 15:40:37 -0400

    Made an edit to Prep 1 and finished Prep 2.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompts the user to input their first name.
+        Console.Write("What is your first name? ");
+        // Returns the user's first name into a string variable from what they typed in.
+        string firstName = Console.ReadLine();
+
+        // Prompts the user to input their first name.
+        Console.Write("What is your last name? ");
+        // Returns the user's last name into a string variable from what they typed in.
+        string lastName = Console.ReadLine();
+
+        // Uses variables inside a string to print the user's name, James Bond style.
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Prompts the user to input a number percentage. Does not work with decimals (yet).
+        Console.Write("Please enter your grade percentage: ");
+        // Returns the user's first name into a string variable from what they typed in.
+        string userInput = Console.ReadLine();
+        // Converts the value of the userInput variable from a string to an interger.
+        int grade = int.Parse(userInput);
+        // Creates the letter variable.
+        string letter = "";
+
+        // if and else if statements to determine the letter grade based off the user's input.
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Prints the letter grade that was determined by the statements above.
+        Console.WriteLine($"Your grade is: {letter}");
+
+        // if and else statement to determine if the grade is a passing grade or not.
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, but that isn't a passing grade.");
+        }
     }
 }
