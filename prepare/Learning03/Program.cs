82547be24023e8a3291032fc647f2c4fcab45db4
Robert Whisenhunt <dahweirdacus@gmail.com>
2025-10-16 14:59:27 -0400

    Made sure the Fraction program worked before pushing it.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        Fraction frac2 = new Fraction(5);
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(3, 4);
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        Fraction frac4 = new Fraction(1, 3);
+        Console.WriteLine(frac4.GetFractionString());
+        Console.WriteLine(frac4.GetDecimalValue());
     }
 }
